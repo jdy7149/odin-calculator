4ba487c179567c2e1ebe23cdcf669b390cb93c0e
jdy7149 <184757480+jdy7149@users.noreply.github.com>
2025-07-18 18:51:20 +0900

    End operate()

---
+++ script.js
@@ -0,0 +1,21 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+let operand1;
+let operator;
+let operand2;
+
+const operate = (operand1, operator, operand2) => {
+    switch (operator) {
+        case '+':
+            return add(operand1, operand2);
+        case '-':
+            return subtract(operand1, operand2);
+        case '*':
+            return multiply(operand1, operand2);
+        case '/':
+            return divide(operand1, operand2);
+    }
+};
